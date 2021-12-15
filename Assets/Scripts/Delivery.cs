b43d942f790ce26679c331614120d3f630ba3bd3
MecIntyre <abuesswalker@gmail.com>
2021-12-15 12:22:46 +0100

    color change if package collect

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32 (1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32 (1, 1, 1, 1);
     [SerializeField] float destroyDelay = 0.5f;
     bool hasPackage;
+
+    SpriteRenderer spriteRenderer;
+
+    private void Start() 
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     private void OnCollisionEnter2D(Collision2D other) 
     {
-        Debug.Log("Bump!");
+        Debug.Log("Boop!");
     }
 
     private void OnTriggerEnter2D(Collider2D other) 
@@ -17,6 +27,7 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package picked up!");
             hasPackage = true;
+            spriteRenderer.color = hasPackageColor;
             Destroy(other.gameObject, destroyDelay);
         }
         
@@ -24,6 +35,7 @@ public class Delivery : MonoBehaviour
         {
             Debug.Log("Package has been delivered!");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
