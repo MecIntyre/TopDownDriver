dd8c2fa176002a613e5dea5efc9d71ad5c9dfff1
MecIntyre <abuesswalker@gmail.com>
2021-12-15 10:25:32 +0100

    add collect, deliver and conditions to logic

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    [SerializeField] float destroyDelay = 0.5f;
+    bool hasPackage;
     private void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log("Bump!");
@@ -11,6 +13,17 @@ public class Delivery : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
-        Debug.Log("Boop!");
+        if (other.tag == "Package" && !hasPackage)
+        {
+            Debug.Log("Package picked up!");
+            hasPackage = true;
+            Destroy(other.gameObject, destroyDelay);
+        }
+        
+        if (other.tag == "Customer" && hasPackage)
+        {
+            Debug.Log("Package has been delivered!");
+            hasPackage = false;
+        }
     }
 }
